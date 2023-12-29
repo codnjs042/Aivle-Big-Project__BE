166576c72850b6d2cd2fa0b8c14b6d85bdb5a80f
ChoHyeonWoo <hwcho0456@gmail.com>
2023-12-29 16:25:54 +0900

    fix: fix shorts url

--- shorts/urls.py
+++ shorts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ShortsView, StreamShortFileView
 
 urlpatterns = [
-    path('shorts/', ShortsView.as_view(), name='shorts'),
+    path('', ShortsView.as_view(), name='shorts'),
     path('stream/', StreamShortFileView.as_view(), name='short_stream'),
 ]
