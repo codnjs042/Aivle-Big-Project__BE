13a14cd7f094d144a5c24e62a131646da3b93e8d
soeun <whthdms9@gmail.com>
2024-01-04 14:40:42 +0900

    fix: fix shorts list api

--- shorts/serializers.py
+++ shorts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import *
+from shorts.models import ShortForm
+from user.models import User
 
 class ShortFormSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,8 +8,12 @@ class ShortFormSerializer(serializers.ModelSerializer):
         fields = ('title', 'file_path')
 
 class ShortsListSerializer(serializers.ModelSerializer):
-    author_name = serializers.ReadOnlyField(source='user.nickname')  # 작성자의 username 필드를 반환
+    # author_name = serializers.ReadOnlyField(source='user.nickname')  # 작성자의 username 필드를 반환
+    author_name = serializers.SerializerMethodField()
     
     class Meta:
         model = ShortForm
-        fields = ('id', 'title', 'view', 'created_at', 'author_name')
+        fields = ('id', 'title', 'author_name', 'view', 'created_at')
+
+    def get_author_name(self, obj):
+        return obj.author.nickname
