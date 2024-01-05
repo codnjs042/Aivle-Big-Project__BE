6257a53b3719ebee9f59f35969670ff7f2a89805
soeun <whthdms9@gmail.com>
2024-01-05 16:00:46 +0900

    fix: fix media, shorts admin

--- backend/urls.py
+++ backend/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +28,4 @@ urlpatterns = [
     path('api/introduce/', include('introduce.urls')),
     path('api/shorts/', include('shorts.urls')),
     path('api/study/', include('study.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shorts/admin.py
+++ shorts/admin.py
@@ -3,7 +3,10 @@ from .models import *
 
 # Register your models here.
 class ShortFormAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'file_path']
+    list_display = ['id', 'author_name', 'title', 'file_path']
     list_display_links = ['id', 'title']
+    
+    def author_name(self, instance):
+        return instance.author.nickname
     
 admin.site.register(ShortForm, ShortFormAdmin)
