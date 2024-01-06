a108733af3ed08302b91e64d160aa4300eb5687b
ChoHyeonwoo <hwcho0456@gmail.com>
2024-01-06 16:45:26 +0900

    feat: change CORS

--- backend/settings.py
+++ backend/settings.py
@@ -61,7 +61,8 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1:3000',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://1.245.237.91:3000',
 ]
 CORS_ALLOW_CREDENTIALS = True
 
