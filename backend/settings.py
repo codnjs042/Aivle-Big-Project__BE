cd0b378e36ae18f72ab442384ccfb3751783b257
ChoHyeonWoo <hwcho0456@gmail.com>
2024-01-07 14:37:28 +0900

    fix: fix notice

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$2k3j%o%t7@d5xtebd&tdlmj816#htg27rt7sr6webyxra1qs=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["1.245.237.91", "localhost"]
+ALLOWED_HOSTS = ["1.245.237.91", "localhost", "127.0.0.1"]
 
 # Application definition
 
