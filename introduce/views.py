3d1feb757af90d4b0e472c3defc187b021867738
soeun <whthdms9@gmail.com>
2024-01-09 17:38:02 +0900

    merge

--- introduce/views.py
+++ introduce/views.py
@@ -1,14 +1,16 @@
+from django.db.models import Case, IntegerField, Value, When
 from django.shortcuts import get_object_or_404
+from drf_spectacular.utils import extend_schema
 from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from drf_spectacular.utils import extend_schema
-from .models import Post
-from .serializers import PostListSerializer, \
+
+from .models import Comment, Post
+from .serializers import CommentSerializer, PostListSerializer, \
     PostSerializer
-from django.db.models import Case, When, Value, IntegerField
-from rest_framework.pagination import PageNumberPagination
+
 
 class PostListView(APIView):
     permission_classes = [IsAuthenticated]
@@ -26,14 +28,17 @@ class PostListView(APIView):
         ).order_by('-is_admin_order')
         page = paginator.paginate_queryset(posts, request)
         if page is not None:
-            serializer = PostListSerializer(page, many=True, context={'request': request})
+            serializer = PostListSerializer(page, many=True,
+                                            context={'request': request})
             return paginator.get_paginated_response(serializer.data)
-        serializer = PostListSerializer(posts, many=True, context={'request': request})
+        serializer = PostListSerializer(posts, many=True,
+                                        context={'request': request})
         return Response(serializer.data)
 
     @extend_schema(request=PostSerializer)
     def post(self, request):
-        serializer = PostSerializer(data=request.data, context={'request': request})
+        serializer = PostSerializer(data=request.data,
+                                    context={'request': request})
         print(request.headers)
         print(request.data)
         if serializer.is_valid():
@@ -62,5 +67,33 @@ class PostView(APIView):
     def delete(self, request, pk):
         notice = get_object_or_404(Post, pk=pk)
         notice.delete()
+        return Response({'message': '삭제되었습니다.'},
+                        status=status.HTTP_204_NO_CONTENT)
+
+
+class CommentView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(request=CommentSerializer)
+    def post(self, request, pk):
+        serializer = CommentSerializer(data=request.data,
+                                    context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @extend_schema(request=CommentSerializer)
+    def patch(self, request, pk):
+        comment = get_object_or_404(Comment, pk=pk)
+        serializer = CommentSerializer(comment, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        comment = get_object_or_404(Comment, pk=pk)
+        comment.delete()
         return Response({'message': '삭제되었습니다.'},
                         status=status.HTTP_204_NO_CONTENT)
