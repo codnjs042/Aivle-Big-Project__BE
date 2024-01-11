5efaa7b00574cb363e77378a14c483eda73e4320
soeun <whthdms9@gmail.com>
2024-01-11 22:47:33 +0900

    fix: fix shorts db

--- shorts/admin.py
+++ shorts/admin.py
@@ -3,8 +3,8 @@ from .models import *
 
 # Register your models here.
 class ShortFormAdmin(admin.ModelAdmin):
-    list_display = ['id', 'author_name', 'title', 'file_path']
-    list_display_links = ['id', 'title']
+    list_display = ['id', 'author_name', 'title', 'file_path', 'view', 'created_at']
+    list_display_links = ['id', 'author_name', 'title']
     
     def author_name(self, instance):
         return instance.author.nickname
